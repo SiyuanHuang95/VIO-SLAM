9190d776c20ddda959312c140d409e34da436afe
uyhsp <siyuan.huang@student.kit.edu>
2020-01-14 23:30:24 +0100

    finish week4 hw

--- week4_sliding_window_optimization/nullspace_test/hessian_nullspace_test.cpp
+++ week4_sliding_window_optimization/nullspace_test/hessian_nullspace_test.cpp
@@ -47,7 +47,7 @@ int main()
         double tx = xy_rand(generator);
         double ty = xy_rand(generator);
         double tz = z_rand(generator);
-
+rec
         Eigen::Vector3d Pw(tx, ty, tz);
         points.push_back(Pw);
 
@@ -60,16 +60,18 @@ int main()
             double z = Pc.z();
             double z_2 = z * z;
             Eigen::Matrix<double,2,3> jacobian_uv_Pc;
+            //误差关于投影点的导数, 详见slam14讲 P168
             jacobian_uv_Pc<< fx/z, 0 , -x * fx/z_2,
                     0, fy/z, -y * fy/z_2;
             Eigen::Matrix<double,2,3> jacobian_Pj = jacobian_uv_Pc * Rcw;
             Eigen::Matrix<double,2,6> jacobian_Ti;
             jacobian_Ti << -x* y * fx/z_2, (1+ x*x/z_2)*fx, -y/z*fx, fx/z, 0 , -x * fx/z_2,
                             -(1+y*y/z_2)*fy, x*y/z_2 * fy, x/z * fy, 0,fy/z, -y * fy/z_2;
+            // 重投影误差关于相机位姿李代数的一阶变化
 
             H.block(i*6,i*6,6,6) += jacobian_Ti.transpose() * jacobian_Ti;
-            /// 请补充完整作业信息矩阵块的计算
-             H.block(j*3 + 6*poseNums,j*3 + 6*poseNums,3,3) += jacobian_Pj.transpose() * jacobian_Pj;
+
+            H.block(j*3 + 6*poseNums,j*3 + 6*poseNums,3,3) += jacobian_Pj.transpose() * jacobian_Pj;
             H.block(i*6,j*3 + 6*poseNums, 6,3) += jacobian_Ti.transpose() * jacobian_Pj;
             H.block(j*3 + 6*poseNums,i*6 , 3,6) += jacobian_Pj.transpose() * jacobian_Ti;
         }
@@ -81,6 +83,6 @@ int main()
 
     Eigen::JacobiSVD<Eigen::MatrixXd> svd(H, Eigen::ComputeThinU | Eigen::ComputeThinV);
     std::cout << svd.singularValues() <<std::endl;
-  
+
     return 0;
 }
